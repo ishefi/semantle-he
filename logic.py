567733e285574df8c80dd4bdd3b6619a34fbfc48
Itamar Shefi <itamarshefi@gmail.com>
2022-03-08 04:47:03 +0200

    make sure bugs like 2000 cache entries does not happen

--- logic.py
+++ logic.py
@@ -141,6 +141,7 @@ class CacheSecretLogic:
 
     def do_populate(self):
         expiration = self.date_ - datetime.utcnow().date() + timedelta(days=4)
+        self.redis.delete(self.secret_cache_key)
         self.redis.rpush(self.secret_cache_key, *self.cache)
         self.redis.expire(self.secret_cache_key, expiration)
         self.vector_logic.secret_logic.set_secret(self.secret)

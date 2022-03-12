49d7eae15576769e1a11a5f0f7ca5aa3b561742f
Itamar Shefi <itamarshefi@gmail.com>
2022-03-12 13:33:38 +0200

    set secret with random-word capabilities

--- scripts/set_secret.py
+++ scripts/set_secret.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from argparse import ArgumentTypeError
 from datetime import datetime
 import os
+import random
 import sys
 
 base = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -22,7 +23,12 @@ def valid_date(date_str):
 
 def main():
     parser = ArgumentParser("Set SematleHe secret for any date")
-    parser.add_argument('secret', metavar='SECRET', help="Secret to set")
+    parser.add_argument(
+        '-s',
+        '--secret',
+        metavar='SECRET',
+        help="Secret to set. If not provided, chooses a random word from Wikipedia.",
+    )
     parser.add_argument(
         '-d', '--date', metavar='DATE', type=valid_date, help="Date of secret. If not provided today's date is used"
     )
@@ -38,11 +44,19 @@ def main():
     mongo = get_mongo()
     redis = get_redis()
 
-    if args.model:
-        logic = CacheSecretLogicGensim(args.model, mongo, redis, args.secret, args.date)
+    if args.secret:
+        secret = args.secret
     else:
-        logic = CacheSecretLogic(mongo, redis, args.secret, args.date)
-    logic.set_secret(dry=True, force=args.force)
+        secret = get_random_word(mongo)
+    do_populate(mongo, redis, args.model, secret, args.date, args.force)
+
+
+def do_populate(mongo, redis, model, secret, date, force):
+    if model:
+        logic = CacheSecretLogicGensim(model, mongo, redis, secret, date)
+    else:
+        logic = CacheSecretLogic(mongo, redis, secret, date)
+    logic.set_secret(dry=True, force=force)
     cache = logic.cache[::-1]
     print(cache)
     for rng in (range(i, i+10) for i in [1, 50, 100, 300, 550, 750]):
@@ -52,6 +66,20 @@ def main():
     pop = input("Populate?\n")
     if pop in ('y', 'Y'):
         logic.do_populate()
+        print("Done!")
+        exit(0)
+    else:
+        secret = get_random_word(mongo)
+        do_populate(mongo, redis, model, secret, date, force)
+
+
+def get_random_word(mongo):
+    while True:
+        secrets = mongo.aggregate([{'$sample': {'size': 100}}])
+        for doc in secrets:
+            secret = doc['word']
+            if input(f'I chose {secret[::-1]}. Ok? [Ny] > ') in ('y', 'Y'):
+                return secret
 
 
 if __name__ == '__main__':

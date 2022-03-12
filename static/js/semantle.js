d24f85c6ac0af26be58505c3a92f1518a891f934
Itamar Shefi <itamarshefi@gmail.com>
2022-03-12 10:14:21 +0200

    hopefully fixed bug in page loading

--- static/js/semantle.js
+++ static/js/semantle.js
@@ -67,6 +67,36 @@ let Semantle = (function() {
     const initialDay = 19044;
     const puzzleNumber = today + 1 - initialDay;
 
+    function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
+    includeHTML();
+
     async function getSim(word) {
         if (cache.hasOwnProperty(word)) {
             return cache[word];
@@ -178,36 +208,6 @@ let Semantle = (function() {
             darkModeCheckbox.checked = on;
         }
     }
-
-    function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
-    }
-  }
-}
-
-    includeHTML();
 
     async function init() {
         if (!storage.getItem("readRules")) {
@@ -403,4 +403,16 @@ Stats (since we started recording, on day 23): <br/>
 // a flash of unstyled content
 Semantle.checkMedia();
 
-window.addEventListener('load', async () => { Semantle.init() });
+const observer = new MutationObserver((mutations, obs) => {
+  let includees = document.querySelectorAll('[w3-include-html]');
+  if (includees.length == 0) {
+    Semantle.init();
+    obs.disconnect();
+    return;
+  }
+});
+
+observer.observe(document, {
+  childList: true,
+  subtree: true
+});

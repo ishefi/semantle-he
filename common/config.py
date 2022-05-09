35b1c3cee3559408274b71b22890f0d7b16c352b
iddoyadlin <iddoyadlin@gmail.com>
2022-05-09 10:51:57 +0300

    putting topups back changing from example to format

--- common/config.py
+++ common/config.py
@@ -1,8 +1,7 @@
 import os
 import sys
 from omegaconf import OmegaConf
-
-OmegaConf.register_new_resolver("defau", lambda x, y: os.environ[x] if x in os.environ else y)
+from pathlib import Path
 
 THIS = os.path.realpath(__file__)
 HERE = os.path.dirname(THIS)
@@ -10,7 +9,7 @@ BASE = os.path.dirname(HERE)
 
 thismodule = sys.modules[__name__]
 
-from pathlib import Path
+
 
 config_path = Path(BASE) / 'config.yaml'
 if config_path.exists():
@@ -18,5 +17,18 @@ if config_path.exists():
     for k, v in conf.items():
         setattr(thismodule, k, v)
 
+def get_top_ups(conf):
+    top_ups = ""
+    for key, value in conf.items():
+        if key.startswith('hs_top_up_'):
+            kkey = key.replace('hs_top_up_', '')
+            try:
+                top_ups += "%s = %r\n" % (kkey, os.environ[value])
+            except KeyError:
+                print('Could not configure %s: %s' % (key, value))
+    return top_ups
+
 if config := os.environ.get("HS_CONFIG"):
     exec(config, globals())
+    top_ups = get_top_ups(globals())
+    exec(top_ups, globals())

5d2f7a9da6369e1966e1e1e26a9edbf0e10c2d35
iddoyadlin <iddoyadlin@gmail.com>
2022-06-19 12:03:39 +0300

    changing yaml to json. yaml file requires break lines, so not possible to pass config as environment variable

--- common/config.py
+++ common/config.py
@@ -7,26 +7,8 @@ thismodule = sys.modules[__name__]
 
 conf = OmegaConf.create()
 if (config_path := Path(__file__).parent.parent.resolve() / 'config.yaml').exists():
-    conf.merge_with(OmegaConf.load(str(config_path)))
+    conf.merge_with(OmegaConf.load(config_path))
 if yaml_str := os.environ.get('YAML_CONFIG_STR'):
     conf.merge_with(OmegaConf.create(yaml_str))
 for k, v in conf.items():
     setattr(thismodule, k, v)
-
-
-def get_top_ups(conf):
-    top_ups = ""
-    for key, value in conf.items():
-        if key.startswith('hs_top_up_'):
-            kkey = key.replace('hs_top_up_', '')
-            try:
-                top_ups += "%s = %r\n" % (kkey, os.environ[value])
-            except KeyError:
-                print('Could not configure %s: %s' % (key, value))
-    return top_ups
-
-
-if config := os.environ.get("HS_CONFIG"):
-    exec(config, globals())
-    top_ups = get_top_ups(globals())
-    exec(top_ups, globals())

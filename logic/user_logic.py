997433776059fafbaa75569d21000b2dd7171623
Itamar Shefi <itamarshefi@gmail.com>
2023-09-27 12:30:52 +0300

    Added user statistics

--- logic/user_logic.py
+++ logic/user_logic.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import datetime
+import sys
+from typing import Optional
 
 from common import config
 from common import schemas
@@ -95,3 +97,56 @@ class UserHistoryLogic:
                 },
             )
         return history
+
+
+class UserStatisticsLogic:
+    def __init__(self, mongo, user):
+        self.mongo = mongo
+        self.user = user
+
+    async def get_statistics(self) -> schemas.UserStatistics:
+        # TODO: for now this is good enough, but we can do it with aggregation.
+        # we should probably change the way we save the data - instead of having
+        # saving history as an object with dates as its members, it should consist of
+        # list with dates as its members.
+        user_history = (
+            await self.mongo.users.find_one(
+                {"email": self.user["email"]}, projection={"history": 1}
+            )
+        ).get("history", {})
+        user_history = {
+            date: [schemas.DistanceResponse(**guess) for guess in history]
+            for date, history in user_history.items() if history
+        }
+
+        game_streak = self._get_game_streak(user_history.keys())
+
+        highest_rank = None
+        total_games_won = 0
+        total_guesses = 0
+        for historia in user_history.values():
+            for guess in historia:
+                if guess.similarity == 100:
+                    total_games_won += 1
+                    highest_rank = min(highest_rank or sys.maxsize, guess.solver_count)
+                    total_guesses += guess.guess_number
+
+        return schemas.UserStatistics(
+            game_streak=game_streak,
+            highest_rank=highest_rank,
+            total_games_played=len(user_history),
+            total_games_won=total_games_won,
+            average_guesses=total_guesses / total_games_won if total_games_won else 0,
+        )
+
+    def _get_game_streak(self, game_dates):
+        game_dates = sorted(game_dates, reverse=True)
+        date = datetime.datetime.utcnow().date()
+        game_streak = 0
+        for game_date in game_dates:
+            if str(date) == game_date:
+                game_streak += 1
+                date -= datetime.timedelta(days=1)
+            else:
+                break
+        return game_streak

dc32f850e6629e44683cd32f958fd0f187e77efc
Itamar Shefi <itamarshefi@gmail.com>
2024-01-14 18:21:10 +0200

    secret word model & table

---
+++ alembic/versions/734535a7c997_secret_word_table.py
@@ -0,0 +1,36 @@
+"""Secret word table
+
+Revision ID: 734535a7c997
+Revises: 23e3fa471b40
+Create Date: 2024-01-14 18:09:54.747781
+
+"""
+from typing import Sequence
+from typing import Union
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "734535a7c997"
+down_revision: Union[str, None] = "23e3fa471b40"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "secretword",
+        sa.Column("id", sa.Integer(), nullable=False, autoincrement=True),
+        sa.Column("word", sa.String(), nullable=False),
+        sa.Column("game_date", sa.Date(), nullable=False),
+        sa.Column("solver_count", sa.Integer(), nullable=False, default=0),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("game_date"),
+        sa.UniqueConstraint("word"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("secretword")

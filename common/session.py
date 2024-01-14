1c3ec4e237ba9af5111095d08411bd020242561d
Itamar Shefi <itamarshefi@gmail.com>
2024-01-14 18:07:58 +0200

    Enforce foreign keys in sqlite

--- common/session.py
+++ common/session.py
@@ -4,6 +4,7 @@ from contextlib import contextmanager
 from typing import TYPE_CHECKING
 
 import gensim.models.keyedvectors as word2vec
+import sqlalchemy
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 from redis.asyncio import Redis
 from sqlmodel import Session
@@ -13,6 +14,7 @@ from common import config
 from model import GensimModel
 
 if TYPE_CHECKING:
+    import sqlite3
     from typing import Any
     from typing import Iterator
 
@@ -31,8 +33,13 @@ def get_model() -> GensimModel:
     return GensimModel(word2vec.KeyedVectors.load("model.mdl").wv)
 
 
+def _fk_pragma_on_connect(dbapi_con: sqlite3.Connection, _: Any) -> None:
+    dbapi_con.execute("PRAGMA foreign_keys=ON")
+
+
 def get_session() -> Session:
     engine = create_engine(config.db_url)
+    sqlalchemy.event.listen(engine, "connect", _fk_pragma_on_connect)
     return Session(engine)
 
 

fa7adb535a9e22f0f8a7fa3cb19b3815138c1c42
Itamar Shefi <itamarshefi@gmail.com>
2024-01-15 12:53:20 +0200

    Use Dropbox API properly

--- download_model.py
+++ download_model.py
@@ -5,7 +5,11 @@ import dropbox
 from common import config
 
 if __name__ == "__main__":
-    client = dropbox.Dropbox(config.dropbox_token)
+    client = dropbox.Dropbox(
+        app_key=config.dropbox["key"],
+        app_secret=config.dropbox["secret"],
+        oauth2_refresh_token=config.dropbox["refresh_token"],
+    )
     destination = "model.zip"
     _, response = client.files_download("/model.zip")
     with open(destination, "wb") as f:
